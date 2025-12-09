37577f6a9494aba8d4ce51194534bdd60e05ece5
Rahmatjon Web <rahmatjon_web@example.com>
2025-12-10 02:46:32 +0500

    Wed Dec 10 2025 02:46:32 GMT+0500 (Uzbekistan Standard Time)

--- gitpush.js
+++ gitpush.js
@@ -4,7 +4,7 @@ const path = require("path");
 const repoPath = path.resolve(__dirname); 
 
 function gitPush() {
-    const commitMessage = `${new Date().toISOString()}`;
+    const commitMessage = `${new Date().toString()}`;
     const command = `git add . && git commit -m "${commitMessage}" && git push origin main`;
 
     exec(command, { cwd: repoPath }, (err, stdout, stderr) => {
